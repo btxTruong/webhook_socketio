64a97ec2f3fffe07e3b2fd72b12049aafbf10365
truongbui <btxtruong@gmail.com>
2024-10-31 13:55:06 +0700

    fix: update websocket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,7 @@
 import {io} from 'socket.io-client';
 
-export const socket = io('http://localhost:3000', {
+import globalConfig from '../../global_config.json';
+
+export const socket = io(`http://localhost:${globalConfig.apiWebsocketPort}`, {
   autoConnect: false,
 });
